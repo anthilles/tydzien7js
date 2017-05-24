b71c280edab27636497a2355fe813cc7af224f5d
anthilles <anthilles@outlook.com>
2017-05-24 15:33:24 +0200

    Zadanie 3 - komentarze w skrypcie "zadanie trzecie"

---
+++ zadanie3/js/zadanietrzecie.js
@@ -0,0 +1,7 @@
+//Pierwszy inny komentarz
+var name = prompt('Proszę tutaj nie wpisywać swojego imienia');
+//alert('Hej! ' + name + ' przecież prosiłem!');
+console.log(name + ' - człowiek, który zagląda wszędzie');
+/*Drugi
+inny
+komentarz*/
