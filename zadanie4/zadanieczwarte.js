13a37fd8df5dc77bf1a68dc2764cbf35b1e44ff6
anthilles <anthilles@outlook.com>
2017-05-25 11:44:40 +0200

    Zadanie czwarte - obliczanie pp trójkąta

---
+++ zadanie4/zadanieczwarte.js
@@ -0,0 +1,5 @@
+var a = prompt('Wpisz długość podstawy trójkąta'),
+    h = prompt('Wpisz wysokość trójkąta'),
+    pole = a * h / 2;
+
+console.log('Pole trójkąta o podstawie: ' + a + ' i wysokości: ' + h + ' wynosi: ' + pole);
